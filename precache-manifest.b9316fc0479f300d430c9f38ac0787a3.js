ccd313970249aed2bc289bbaff34c5ce708badcb
Jonah Householder <jonahhouseholder@outlook.com>
2020-07-04 12:11:56 -0700

    Updates

---
+++ precache-manifest.b9316fc0479f300d430c9f38ac0787a3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fa71086e973cc346d61d8855f649cf",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "8f428785c7ffebb40aad",
+    "url": "/reactPortfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cd5a851bdb6a42f94eaf",
+    "url": "/reactPortfolio/static/css/main.86c0f29c.chunk.css"
+  },
+  {
+    "revision": "8f428785c7ffebb40aad",
+    "url": "/reactPortfolio/static/js/2.eeefa797.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactPortfolio/static/js/2.eeefa797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5a851bdb6a42f94eaf",
+    "url": "/reactPortfolio/static/js/main.24b63e94.chunk.js"
+  },
+  {
+    "revision": "5beb382b5605ad57bd8b",
+    "url": "/reactPortfolio/static/js/runtime-main.01400207.js"
+  },
+  {
+    "revision": "3f62b5862ab1cb5e324cefbf6887f840",
+    "url": "/reactPortfolio/static/media/Listivity.3f62b586.png"
+  },
+  {
+    "revision": "697ace78ad608461f2ae796e20f9a7ce",
+    "url": "/reactPortfolio/static/media/Listivity.697ace78.gif"
+  },
+  {
+    "revision": "9ed7eaa953030900f0b60aaa90f391c2",
+    "url": "/reactPortfolio/static/media/LittleGems.9ed7eaa9.gif"
+  },
+  {
+    "revision": "c322d0a96c98c29bbbdaae5688f89078",
+    "url": "/reactPortfolio/static/media/LittleGems.c322d0a9.png"
+  },
+  {
+    "revision": "96c5e55ad6ab9b2801ef320773a8d819",
+    "url": "/reactPortfolio/static/media/SG-Homepage.96c5e55a.png"
+  }
+]);
